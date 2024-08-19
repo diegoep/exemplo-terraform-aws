cec3f4336db216d28f796a9f89fff515babe8f0e
Diego Pessoa <diego.pessoa@ifpb.edu.br>
2024-08-19 09:20:24 -0300

    adiciona instancia novamente

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
-# output "name" {
-#   value = aws_instance.web.tags.Name
-# }
-# output "public_ip" {
-#   value = aws_instance.web.public_ip
-# }
+output "name" {
+  value = aws_instance.web.tags.Name
+}
+output "public_ip" {
+  value = aws_instance.web.public_ip
+}
